0e114f4a69ba89741f72c3df1e676ccada494b6d
Derick <derick@pop-os.localdomain>
2020-05-14 23:31:57 +0300

    pouchdb-server

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var PouchDB = require('pouchdb');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use('/db', require('express-pouchdb')(PouchDB));
 console.log('Listening at port :' + port);
 
